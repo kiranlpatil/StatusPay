6fda697faff6873765ec0a2df7405bafacc5a42a
Kiran Patil <kiran.patil@techprimelab.com>
2022-01-15 17:35:49 +0530

    Dashboard Screen

--- App.js
+++ App.js
@@ -11,9 +11,9 @@ const {Navigator, Screen} = createStackNavigator();
 
 const StackNavigator = () => (
   <Navigator screenOptions={{headerShown: false}}>
-    <Screen name="Login" component={LoginScreen} />
+    {/* <Screen name="Login" component={LoginScreen} />
     <Screen name="OtpVerify" component={OtpScreen} />
-    <Screen name="Signup Page" component={SignUp} />
+    <Screen name="Signup Page" component={SignUp} /> */}
     <Screen name="Dashboard" component={DashboardScreen} />
     <Screen name="AdCampaign" component={AdCampaign} />
   </Navigator>
